6fbc0573b81a1cb9998b2dc88c32e844750f31fc
Abhay-Rana-18 <ranaabhay795@gmail.com>
2024-07-07 20:48:39 +0530

    "app.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,39 @@ import {
   createBrowserRouter,
   Outlet,
 } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import HomePage from "./pages/HomePage.jsx";
+
+import RegisterPage from "./pages/User/RegisterPage";
+
 import BeerForm from "./pages/Beer/BeerForm.jsx";
 import LiquorForm from "./pages/Liquor/LiquorForm.jsx";
 import CompanyForm from "./pages/Company/CompanyForm.jsx";
 import CompanyList from "./pages/Company/CompanyList.jsx";
+import HomePage from "./Homepage/HomePage.jsx";
 
+import Navbar from "./components/Navbar.jsx";
+import LoginPage from "./pages/User/LoginPage.jsx";
+import BillRecords from "./pages/Bill/BillRecords.jsx";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/beer/create" element={<BeerForm />} />
-        <Route path="/liquor/create" element={<LiquorForm />} />
-        <Route path="/company" element={<CompanyList />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/bill/records" element={<BillRecords />} />
+          <Route path="/beer/create" element={<BeerForm />} />
+          <Route path="/liquor/create" element={<LiquorForm />} />
+          <Route path="/company" element={<CompanyList />} />
+        </Route>
       </Route>
     )
   );
