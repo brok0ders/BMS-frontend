8942eab91304d6ff5850b5daef06dfde73f8f892
Mukesh Pandey <panditmukki5@gmail.com>
2025-04-12 09:54:50 +0530

    feat: Update dependencies and enhance date range selection in analytics
    - Added @mui/base dependency for improved Material UI components.
    - Upgraded apexcharts to version 4.5.0 for better charting capabilities.
    - Introduced react-date-range for date selection in MonthlyAnalytics component.
    - Refactored MonthlyAnalytics to use new date range picker and improved date handling.
    - Cleaned up unused imports and commented-out code for better readability.
    - Adjusted styles in index.css for gradient background.
    - Modified AnalyticsCard to improve responsiveness and layout.

--- src/pages/Analytics/AnalyticsCard.jsx
+++ src/pages/Analytics/AnalyticsCard.jsx
@@ -8,11 +8,11 @@ const AnalyticsCard = ({ name, value, icon }) => {
           {name}
         </h1>
         <div className="w-[4rem] shadow-md shadow-blue-400/30 h-[4rem] rounded-full bg-yellow-50/50 p-3">
-        {/* <div className="w-[4rem] shadow-md shadow-blue-400/30 h-[4rem] rounded-full bg-gradient-to-tr from-blue-300 via-gray-50 to-sky-500 p-3"> */}
+          {/* <div className="w-[4rem] shadow-md shadow-blue-400/30 h-[4rem] rounded-full bg-gradient-to-tr from-blue-300 via-gray-50 to-sky-500 p-3"> */}
           <img className="w-full" src={icon} alt="" />
         </div>
       </div>
-      <h1 className="text-xl md:text-3xl font-semibold text-gray-600">
+      <h1 className="text-xl md:text-2xl font-semibold text-gray-600">
         {value || 0}
       </h1>
     </div>
